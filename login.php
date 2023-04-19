388ced48abccb03a81e85c0a5c9df04be78376c6
Your Name <you@example.com>
2023-04-19 23:58:39 +0800

    modify element

--- login.php
+++ login.php
@@ -49,7 +49,7 @@ if(isset($_POST['tb_customer'])){
 <head>
     <meta charset="utf-8">
     <title>Login | RENTALJO</title>
-    <link rel="stylesheet" href="login.css?v=2">
+    <link rel="stylesheet" href="login.css">
     <!-- <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
@@ -57,30 +57,32 @@ if(isset($_POST['tb_customer'])){
 
 <body>
     <div class="wrapper d-flex">
-        <div class="title d-flex justify-content-center align-items-center">
+        <div class="title col-md-5">
             <img src="img/logo1.png" alt="logo" style="width:280px; height:280px;">
         </div>
-        <form id="form_login" role="form" action="" method="post">
-        <?php if($err){ ?>
-        <div id="login-alert" class="alert alert-danger col-sm-12">
-            <p><?php echo $err ?></p>
+        <div class="col-md-7">
+            <form id="form_login" role="form" action="" method="post">
+                <?php if($err){ ?>
+                <div class="alert alert-danger d-flex align-items-center" role="alert">
+                    <p><?php echo $err ?></p>
+                </div>
+                <?php } ?>
+                <div class="field">
+                    <input type="text" id="login_email" name="username">
+                    <label>Username</label>
+                </div>
+                <div class="field">
+                    <input type="password" name="password" id="login_password">
+                    <label>Password</label>
+                </div>
+                <div class="field">
+                    <input type="submit" name="tb_customer">
+                </div>
+                <div class="signup-link">
+                    Don't have an account yet? <a href="regis.php">Signup</a>
+                </div>
+            </form>
         </div>
-        <?php } ?>
-            <div class="field">
-                <input type="text" id="login_email" name="username">
-                <label>Username</label>
-            </div>
-            <div class="field">
-                <input type="password" name="password" id="login_password">
-                <label>Password</label>
-            </div>
-            <div class="field">
-                <input type="submit" name="tb_customer">
-            </div>
-            <div class="signup-link">
-                Not a member? <a href="regis.php">Signup now</a>
-            </div>
-        </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
--- regis.php
+++ regis.php
@@ -4,58 +4,59 @@
 <head>
    <meta charset="utf-8">
    <title>Signup | RENTALJO</title>
-   <link rel="stylesheet" href="regis.css?v=2">
+   <link rel="stylesheet" href="regis.css">
+   <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
       integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 
 <body>
-   <div class="wrapper  d-flex">
-      <div class="title d-flex justify-content-center align-items-center">
+   <div class="wrapper d-flex">
+      <div class="title col-md-6 d-flex justify-content-center">
          <img src="img/logo1.png" alt="logo" style="width:350px; height:350px;">
       </div>
-      <form id="form_login" role="form" method="post" action="config_regis.php">
-         <div class="d-flex">
-            <div class="aa">
-               <div class="field">
-                  <input type="text" name="usrname" id="register_username" required>
-                  <label>Username</label>
+      <div class="col-md-6 ">
+         <form id="form_login" role="form" method="post" action="config_regis.php">
+            <div class="d-flex">
+               <div class="aa">
+                  <div class="field">
+                     <input type="text" name="nama_cus" id="nama_customer" required>
+                     <label>Nama Lengkap</label>
+                  </div>
+                  <div class="field" placeholder="Jenis Kelamin">
+                     <select id="jk" name="jk">
+                        <option value="Pria">Pria</option>
+                        <option value="Wanita">Wanita</option>
+                     </select>
+                  </div>
+                  <div class="field">
+                     <input type="text" name="alamat" id="alamat" required>
+                     <label>Alamat</label>
+                  </div>
                </div>
-               <div class="field">
-                  <input type="password" name="pwd" id="password" required>
-                  <label>Password</label>
-               </div>
-               <div class="field">
-                  <input type="text" name="nama_cus" id="nama_customer" required>
-                  <label>Nama Lengkap</label>
+               <div class="bb">
+                  <div class="field">
+                     <input type="number" name="no_hp" id="no_hp" required>
+                     <label>Nomor Handphone</label>
+                  </div>
+                  <div class="field">
+                     <input type="text" name="usrname" id="register_username" required>
+                     <label>Username</label>
+                  </div>
+                  <div class="field">
+                     <input type="password" name="pwd" id="password" required>
+                     <label>Password</label>
+                  </div>
                </div>
             </div>
-            <div class="bb">
-               <div class="field" placeholder="Jenis Kelamin">
-                  <select id="jk" name="jk">
-                     <!-- <option value=""></option> -->
-                     <option value="Pria">Pria</option>
-                     <option value="Wanita">Wanita</option>
-                  </select>
-                  <!-- <label>Jenis Kelamin</label> -->
-               </div>
-               <div class="field">
-                  <input type="text" name="alamat" id="alamat" required>
-                  <label>Alamat</label>
-               </div>
-               <div class="field">
-                  <input type="number" name="no_hp" id="no_hp" required>
-                  <label>Nomor Handphone</label>
-               </div>
+            <div class="field">
+               <input type="submit">
             </div>
-         </div>
-         <div class="field">
-            <input type="submit">
-         </div>
-         <div class="signup-link">
-            Already a member? <a href="login.php">Login Now</a>
-         </div>
-      </form>
+            <div class="signup-link">
+               Already have an account? <a href="login.php">Login</a>
+            </div>
+         </form>
+      </div>
    </div>
    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
