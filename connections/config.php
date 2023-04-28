9964a941aafefc989064046b2eb3bb93d0de5a80
Your Name <you@example.com>
2023-04-28 23:32:34 +0800

    modify hero

---
+++ connections/config.php
@@ -0,0 +1,9 @@
+<?php
+$servername = "localhost"; //nama server database
+$username = "root"; //nama pengguna database
+$password = ""; //kata sandi database
+$dbname = "db_rental_motor"; //nama database
+
+// Membuat koneksi
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
