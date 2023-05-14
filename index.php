3621dc0839e699d072d3334fb8ee66620b681598
Your Name <you@example.com>
2023-05-14 19:48:00 +0800

    modify admin

--- index.php
+++ index.php
@@ -9,15 +9,12 @@ require_once "connections/config.php";
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Home Page User</title>
+    <title>RENTALJO | Sewa Motor Terbaik</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css?v=2" rel="stylesheet"
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
         <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: #fff !important;">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary" style="background-color: #fff !important;">
         <div class="container">
             <a class="navbar-brand" href="#"> <img style="width: 90px; height: 90px;" src="img/logo1.png"
                     alt="Logo"></a>
@@ -63,6 +60,10 @@ require_once "connections/config.php";
             </div>
         </div>
     </nav>
+</head>
+
+<body>
+    
     
     <?php
     error_reporting(0);
@@ -112,7 +113,7 @@ require_once "connections/config.php";
     </footer>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js?v=2"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
     </script>
 </body>
