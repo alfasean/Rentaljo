b40acdec4a3dd6ed65f040963ea81a240d9692e4
Your Name <you@example.com>
2023-05-20 21:36:16 +0800

    modify service element

--- service.php
+++ service.php
@@ -16,28 +16,28 @@
     <button class="pre-btn"><img src="img/arrow.png" alt="arrow" /></button>
     <button class="nxt-btn"><img src="img/arrow.png" alt="arrow2" /></button>
     <div class="container-services mt-4">
-      <div class="row">
-        <?php
-          $servername = "localhost";
-          $username = "root"; 
-          $password = ""; 
-          $dbname = "db_rental_motor";
-          $conn = new mysqli($servername, $username, $password, $dbname);
-          $query_res = mysqli_query($conn, "SELECT * FROM tb_motor"); 
-          while ($r = mysqli_fetch_array($query_res)) {
-            echo '<div class="col-md-4 mx-5 mb-3">
+      <?php
+        $servername = "localhost";
+        $username = "root"; 
+        $password = ""; 
+        $dbname = "db_rental_motor";
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        $query_res = mysqli_query($conn, "SELECT * FROM tb_motor"); 
+        while ($r = mysqli_fetch_array($query_res)) {
+          echo '<div class="row">
+                  <div class="col-md-4 mx-3 mb-3">
                     <div class="card border-0">
                       <img class="card-img-top mx-auto" alt="img1" src="Admin/uploads/'.$r['gambar'].'" />
                       <div class="card-body">
                         <p class="mx-auto judul">'.$r['merk'].'</p>
-                        <p class="card-text text-center">Rp. '.$r['harga'].'</p>
+                        <p class="card-text text-center">Rp '.$r['harga'].'</p>
                       </div>
                       <a href="#" class="btn sewa_button text-white mx-auto mt-4 mb-4">Sewa Sekarang</a>
                     </div>
-                  </div>';
-          }
-        ?>
-      </div>
+                  </div>
+                </div>';
+        }
+      ?>
     </div>
   </div>
 
